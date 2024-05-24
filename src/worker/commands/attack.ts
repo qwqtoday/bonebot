8a1ac1aefe94592b9fd9d1ea7fbfa1bb151f357d
qwqtoday <lukas.leung@hotmail.com>
2024-05-24 19:32:23 +0800

    update

--- src/worker/commands/attack.ts
+++ src/worker/commands/attack.ts
@@ -8,8 +8,8 @@ export default {
 			context.respond('Invalid delay. usage: attack [delayMS] [entity1] [entity2] [entity3] ....');
 			return;
 		}
-		if (delay < 1000) {
-			context.respond('Delay must be at least 1000 ms');
+		if (delay < 500) {
+			context.respond('Delay must be at least 500 ms');
 			return;
 		}
 		bot.task.tasks['attack'].options.delay = delay;
